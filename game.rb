5fff63efb3c38a6e78e959d2ef4b734468185408
Joshua McGee <jcooler.cooler@gmail.com>
2019-09-16 13:25:03 -0700

    build the initialize in start and created a method called ask_q that loops through asking questions and updating variables until lives reaches 0

--- game.rb
+++ game.rb
@@ -1,8 +1,6 @@
 
 class Player
   #updates and reads the player attributs
-  # include ClassLevelInheritableAttributes
-  # inheritable_attributes :name, :lives
   attr_accessor :name, :lives 
   
   def initialize(name, lives)
@@ -13,34 +11,81 @@ class Player
 end
 
 class Question
-
+  # sends the numbers data and sum of the numbers each time its created
   attr_accessor :num1, :num2, :sum
 
   def initialize
   @num1 = (rand(20))
   @num2 = (rand(20))
-  @sum = num1 + num2
+  @sum = (@num1 + @num2)
   end
   #creates a question passed to class game
-
+  
 end
+
+
 
 class Start
-  #create our classes
-  player1 = Player.new('Player1', 3)
-  player2 = Player.new('Player2', 3)
-  question = Question.new
+  
+ 
+  def initialize
+    #create our classes
+    @player1 = Player.new('Player1', 3)
+    @player2 = Player.new('Player2', 3)
+    @player_turn = 1
+    #@question = Question.new
+    
+  end
 
-  #containes the logic for the game and player input
-  puts "#{player1.name}: What does #{question.num1} plus #{question.num2} equal?"
-  
-  
+  def ask_q
+    #containes the logic for the game and player input
+    while @player1.lives || @player2.live != 0
+      @question = Question.new
+      if @player_turn == 1
+          puts "#{@player1.name}: What does #{@question.num1} plus #{@question.num2} equal?"
+          print "> "
+          choice = gets.chomp
+
+          if choice == "#{@question.sum}"
+            puts "Correct!, time to switch"
+            @player_turn += 1
+          else 
+            @player1.lives -= 1
+            wrong(@player1.lives)
+            @player_turn += 1
+          end
+
+      elsif @player_turn == 2
+        @question = Question.new
+        puts "#{@player2.name}: What does #{@question.num1} plus #{@question.num2} equal?"
+        print "> "
+        choice = gets.chomp
+
+        if choice == "#{@question.sum}"
+          puts "Correct!, time to switch"
+          @player_turn -= 1
+        else 
+          @player2.lives -= 1
+          wrong(@player2.lives)
+          @player_turn -= 1
+        end
+      end
+    end
+  end
+
+  # this checks if the lives are 0 and once they are ends the game
+  def wrong(lives)
+
+    if lives == 0
+      puts "you died (and suck at math)"
+      exit(0)
+    end
+    puts "You have #{lives} lives left, time to switch'"
+  end
+
 end
 
-
-
-# puts player1.name
-# puts player1.lives
-
-Start
+# start the game.
+start = Start.new
+start.ask_q
 
