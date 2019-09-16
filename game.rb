f2a3b6da529fc72d4f5397b6b1b20c035480f4a2
Joshua McGee <jcooler.cooler@gmail.com>
2019-09-16 12:40:41 -0700

    added the Questions calss logic and created first question

--- game.rb
+++ game.rb
@@ -1,22 +1,46 @@
 
 class Player
-  attr_accessor :name, :lives
-
-  def initialize(name)
+  #updates and reads the player attributs
+  # include ClassLevelInheritableAttributes
+  # inheritable_attributes :name, :lives
+  attr_accessor :name, :lives 
+  
+  def initialize(name, lives)
     @name = name
-    @lives = 3
+    @lives = lives
   end
-end
 
-class Game
 end
 
 class Question
+
+  attr_accessor :num1, :num2, :sum
+
+  def initialize
+  @num1 = (rand(20))
+  @num2 = (rand(20))
+  @sum = num1 + num2
+  end
+  #creates a question passed to class game
+
 end
 
-player1 = Player.new('Player1')
-player2 = Player.new('Player2')
+class Start
+  #create our classes
+  player1 = Player.new('Player1', 3)
+  player2 = Player.new('Player2', 3)
+  question = Question.new
 
-puts player1.name
-puts player1.lives
+  #containes the logic for the game and player input
+  puts "#{player1.name}: What does #{question.num1} plus #{question.num2} equal?"
+  
+  
+end
+
+
+
+# puts player1.name
+# puts player1.lives
+
+Start
 
